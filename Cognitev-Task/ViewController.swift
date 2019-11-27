4018e9029bcb875a39ce2826b3087760f40606e8
Ahmed Ramy <Dev.ahmedramy@icloud.com>
2019-11-27 12:34:06 +0200

    Initial Commit

---
+++ Cognitev-Task/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cognitev-Task
+//
+//  Created by Ahmed Ramy on 11/27/19.
+//  Copyright © 2019 Ahmed Ramy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Cognitev-TaskTests/Cognitev_TaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  Cognitev_TaskTests.swift
+//  Cognitev-TaskTests
+//
+//  Created by Ahmed Ramy on 11/27/19.
+//  Copyright © 2019 Ahmed Ramy. All rights reserved.
+//
+
+import XCTest
+@testable import Cognitev_Task
+
+class Cognitev_TaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
